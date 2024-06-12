56abd3668564770ac9df17b28f22c59916ea7e2e
Peter Bui <pbui@nd.edu>
2024-06-12 16:26:10 -0400

    Lecture 09: update bst

--- lecture09/bst_template.py
+++ lecture09/bst_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from dataclasses import dataclass
-from typing      import Optional
+from typing      import Iterator, Optional
 
 import sys
 
@@ -9,51 +9,70 @@ import sys
 
 @dataclass
 class Node:
+    ''' Node Structure '''
     value:  int
     left:   Optional['Node'] = None
     right:  Optional['Node'] = None
 
 @dataclass
 class Tree:
-    root:   Node = None
+    ''' Tree Structure '''
 
-    def search(self, value):
+    root:   Optional[Node] = None
+
+    def search(self, value) -> bool:
+        ''' Determine if value is in tree '''
         pass
 
-    def search_r(self, node, value):
+    def _search(self, node: Optional[Node], value) -> bool:
+        ''' Determine if value is in tree (recursively) '''
         pass
 
-    def insert(self, value):
+    def insert(self, value) -> None:
+        ''' Add value into tree '''
         pass
 
-    def insert_r(self, node, value):
+    def _insert(self, node: Optional[Node], value) -> Node:
+        ''' Add value into tree (recursively) '''
         pass
 
     def print(self):
+        ''' Print all nodes in tree '''
         pass
 
-    def print_r(self, node):
+    def _print(self, node: Optional[Node]):
+        ''' Print all nodes in tree (recursively) '''
         pass
 
-    def nodes(self):
+    def nodes(self) -> Iterator[int]:
+        ''' Generate all nodes in tree '''
         pass
 
-    def nodes_r(self, node):
+    def _nodes(self, node: Optional[Node]) -> Iterator[int]:
+        ''' Generate all nodes in tree (recursively) '''
         pass
 
 # Main Execution
 
-def main():
-    tree = Tree()
-    for number in map(int, sys.argv[1:]):
-        tree.insert(number)
+def main(stream=sys.stdin):
+    ''' For each line, insert each number into tree, search for items, and then
+    print tree '''
 
-    for number in map(int, sys.argv[1:]):
-        print(number, tree.search(number))
-        print(2*number, tree.search(number*2))
+    for line in stream:
+        strs = line.split()                 # Split line into individual strings
+        ints = [int(s) for s in strs]       # Convert each string into an int
+        tree = Tree()
 
-    tree.print()
-    print(', '.join(map(str, tree.nodes())))
+        for number in ints:                 # Insert ints into tree
+            tree.insert(number)
+
+        tree.print()
+
+        for number in range(max(ints) + 1): # Search for ints in tree
+            print(number, tree.search(number))
+
+        for node in tree.nodes():
+            print(node)
 
 if __name__ == '__main__':
     main()
